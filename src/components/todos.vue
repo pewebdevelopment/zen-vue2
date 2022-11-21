beae75817e0dee06939e3e511dd7a61c1f50a66a
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-21 11:16:34 +0530

    Making a ToDos Table

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -66,6 +66,123 @@
       </button>
     </div>
 
+    <!-- Table for Displaying the Todos -->
+
+    <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead
+          class="
+            text-xs text-gray-700
+            uppercase
+            bg-gray-50
+            dark:bg-gray-700 dark:text-gray-400
+          "
+        >
+          <tr>
+            <th scope="col" class="py-3 px-6">Serial Number</th>
+            <th scope="col" class="py-3 px-6">ToDo Name</th>
+
+            <th scope="col" class="py-3 px-6">Action</th>
+            <th scope="col" class="p-4">
+              <div class="flex items-center">
+                <input
+                  id="checkbox-all-search"
+                  type="checkbox"
+                  class="
+                    w-4
+                    h-4
+                    text-blue-600
+                    bg-gray-100
+                    rounded
+                    border-gray-300
+                    focus:ring-blue-500
+                    dark:focus:ring-blue-600 dark:ring-offset-gray-800
+                    focus:ring-2
+                    dark:bg-gray-700 dark:border-gray-600
+                  "
+                />
+                <label for="checkbox-all-search" class="sr-only"
+                  >checkbox</label
+                >
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="
+              bg-white
+              border-b
+              dark:bg-gray-800 dark:border-gray-700
+              hover:bg-gray-50
+              dark:hover:bg-gray-600
+            "
+          >
+            <th
+              scope="row"
+              class="
+                py-4
+                px-6
+                font-medium
+                text-gray-900
+                whitespace-nowrap
+                dark:text-white
+              "
+            >
+              001"
+            </th>
+            <td class="py-4 px-6">Walk the dog</td>
+
+            <td class="flex items-center py-4 px-6 space-x-3">
+              <a
+                href="#"
+                class="
+                  font-medium
+                  text-blue-600
+                  dark:text-blue-500
+                  hover:underline
+                "
+                >Edit</a
+              >
+              <a
+                href="#"
+                class="
+                  font-medium
+                  text-red-600
+                  dark:text-red-500
+                  hover:underline
+                "
+                >Remove</a
+              >
+            </td>
+            <td class="p-4 w-4">
+              <div class="flex items-center">
+                <input
+                  id="checkbox-table-search-1"
+                  type="checkbox"
+                  class="
+                    w-4
+                    h-4
+                    text-blue-600
+                    bg-gray-100
+                    rounded
+                    border-gray-300
+                    focus:ring-blue-500
+                    dark:focus:ring-blue-600 dark:ring-offset-gray-800
+                    focus:ring-2
+                    dark:bg-gray-700 dark:border-gray-600
+                  "
+                />
+                <label for="checkbox-table-search-1" class="sr-only"
+                  >checkbox</label
+                >
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
     <div class="table-container">
       <table>
         <tr>
